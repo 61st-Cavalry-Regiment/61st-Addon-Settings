136b5cfba53d472b5fe0732ccd419751796b43d2
Jacob Oxley <jdoxley6@gmail.com>
2020-07-17 16:20:05 -0700

    Something

---
+++ cba_settings_userconfig/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches {
+    class cba_settings_userconfig {
+        author = "61st Auxillary Mod Team";
+        name = "61st CBA Settings";
+        url = "https://61st-regiment.com";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {"cba_settings"};
+        version = 1.0;
+        authors[] = {"Thunder"};
+    };
+};
